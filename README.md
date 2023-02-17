d9d7bba6ff814a15cb8d193cbdb3f5a7d09bf880
jameel <jameel.geduld@sovtech.com>
2023-02-17 20:46:23 +0200

    frontend ui

--- README.md
+++ README.md
@@ -1,70 +1,74 @@
-# Getting Started with Create React App
+# Prop Listing UI
+## Table of Contents
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+- [Overview](#Overview)
+- [Getting Started](#getting-started)
+- [Docker](#docker)
+- [Contributing to the Project](#contributing-to-the-project)
+- [Other](#other)
+- [Meta](#meta)
 
-## Available Scripts
+## Overview
 
-In the project directory, you can run:
+A frontend ui for displaying property listing, using backend lamdba api built with nestjs api
 
-### `npm start`
+* debounce
+* distinctUntilChanged
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+![UI](prop.png)
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+## Getting Started
 
-### `npm test`
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+### Environment Variables 
+An example of environment variabes is included needed for duplication `.env.example` and copy into `.env`
 
-### `npm run build`
+### Project start
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+#### Install Dependencies
+1. Get Yarn
+``
+    npm -g install yarn
+``
+2. Install Dependencies
+``
+yarn install
+``
+3. Compiles for development
+``
+yarn serve
+``
+4. Compiles and minifies for production
+``
+yarn build
+``
+5. Lints and fixes files
+``
+yarn lint
+``
+## Docker
+This project has been dockerized for purpose of deployment, so proceed with steps below if you consider deploying with docker
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+#### Build Docker Image
+```
+docker build . -t app-ui/health-frontend
+docker run -it -p 8080:80 --env-file=.env --rm --name health-frontend-ui app-ui/health-frontend
+```
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+## Contributing to the Project
 
-### `npm run eject`
+For detailed guidelines on how to create custom branches according to contribution follow
+Refer to `CONTRIBUTING.md`
+## Other
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+See the installation guides available @ nodejs.org:
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+https://nodejs.org/en/download/package-manager/
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
+## Meta
 
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
+| Version | Author                                          | Date       |
+| ------- | ----------------------------------------------- | ---------- |
+| 0.0.1   | Jameel Geduld <jameel.wilite@gmail.com> | 16/02/2023 |
 
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
---
+++ src/components/PropertyCard/index.js
@@ -0,0 +1,170 @@
+import React, { useEffect, useState } from 'react';
+import {
+    Avatar,
+    Badge,
+    Flex,
+    Icon,
+    Image,
+    Text,
+    Box,
+    SkeletonCircle,
+    SkeletonText
+
+} from "@chakra-ui/react";
+
+import { FaBath, FaCar, FaHome } from 'react-icons/fa';
+
+const BACKEND_API = 'https://0bykjr7fn1.execute-api.eu-west-1.amazonaws.com/dev'
+
+function PropertyCard() {
+    const [properties, setProperties] = useState([]);
+
+    const [isLoading, setIsLoading] = useState(false)
+
+    const [apiError, setApiError] = useState(false)
+
+    useEffect(() => {
+        setIsLoading(true);
+        console.log('url', `${BACKEND_API}/listing/all`)
+        fetch(`${BACKEND_API}/listing/all`)
+            .then(res => res.json())
+            .then(response => {
+                console.log(response)
+                setProperties(response);
+                setIsLoading(false);
+            })
+            .catch(error => {
+                console.log(error)
+                setApiError(true)
+                setIsLoading(false);
+            });
+    }, [])
+
+    return (
+        <Flex direction={'row'} flexWrap='wrap'>
+            {isLoading ?
+                <Flex
+                    height={100}>
+
+                    {Array(3).fill().map(skeleton =>
+                        <Box
+                            borderRadius='20px'
+                            p='20px'
+                            h='400px'
+                            w={{ base: "315px", md: "400px" }}
+                            alignItems='center'
+                            direction='column'>
+                            <SkeletonCircle size='10' />
+                            <SkeletonText mt='4' noOfLines={4} spacing='4' skeletonHeight='2' />
+                        </Box>
+                    )}
+
+                </Flex> : <></>}
+            {!isLoading && properties.map(prop =>
+                <Box
+                    p={4}
+                    display={{ md: "flex" }}
+                    maxWidth="32rem"
+                    borderWidth={1}
+                    borderRadius='20px'
+                    margin={2}>
+                    <Flex
+                        borderRadius='20px'
+                        p='20px'
+                        h='400px'
+                        w={{ base: "315px", md: "400px" }}
+                        alignItems='center'
+                        direction='column'>
+                        <Flex w='100%' mb='18px'>
+                            <Text
+                                my='auto'
+                                fontWeight='600'
+                                textAlign='center'
+                                noOfLines={0}
+                                me='auto'>
+                                {prop.title}
+                            </Text>
+                        </Flex>
+                        <Image
+                            src={prop.images[0]}
+                            maxW='100%'
+                            width={"85%"}
+                            borderRadius='20px'
+                            mb='10px'
+                        />
+
+                        <Text
+                            fontWeight='500'
+                            textAlign='start'
+                            fontSize='xl'
+                            w='100%'>
+                            R {prop.unit.price}
+                        </Text>
+                        <Flex mt='auto' justify='space-between' w='100%' align='center'>
+                            <Badge
+                                borderRadius='9px'
+                                size='md'
+                                colorScheme='green'
+                                color='green.400'
+                                textAlign='center'
+                                display='flex'
+                                justifyContent='center'
+                                alignItems='center'>
+                                Property Agent
+                            </Badge>
+
+                            <Avatar
+                                size='xs'
+                                name={prop.organisation.name}
+                                src={prop.organisation.logoUrl} />
+
+                        </Flex>
+                        <Flex
+                            mt='auto' justify='left' w='100%' align='center'
+                        >
+                            <Flex me='25px'>
+                                <Icon as={FaHome} color='grey.100' />
+                                <Text fontSize='sm' my='auto' fontWeight='500'>
+                                    {prop.unit.bedrooms} Bedrooms
+                                </Text>
+                            </Flex>
+                            <Flex me='25px'>
+                                <Icon as={FaBath} color='grey.100' />
+                                <Text fontSize='sm' my='auto' fontWeight='500'>
+                                    {prop.unit.bathrooms} Bathrooms
+                                </Text>
+                            </Flex>
+                            <Flex>
+                                <Icon as={FaCar} color='grey.100' />
+                                <Text fontSize='sm' my='auto' fontWeight='500'>
+                                    {prop.unit.parking} Parking
+                                </Text>
+                            </Flex>
+                        </Flex>
+                        <Flex
+                            my='auto'
+                            fontWeight='600'
+                            textAlign='center'
+                            noOfLines={0}
+                            me='auto'
+                        >
+                            <Text
+                                fontWeight='200'
+                                textAlign='start'
+                                fontSize='sm'
+                                noOfLines={2}
+                                w='100%'>
+                                {prop.description}
+                            </Text>
+                        </Flex>
+                    </Flex>
+                </Box>
+            )}
+
+
+        </Flex>
+
+    );
+}
+
+export default PropertyCard;
